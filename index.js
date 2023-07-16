7c184bad846621c300aff9073b64da7a67871e99
unknown <peter.ostertag99@gmail.com>
2023-07-16 18:40:56 -0500

    Set up skeleton code

---
+++ index.js
@@ -0,0 +1,14 @@
+// Required dependencies
+const fs = require('fs');
+const inquirer = require(inquirer);
+
+function generateLogo() {
+
+}
+
+function getShape() {
+
+}
+
+// Function call
+generateLogo();
