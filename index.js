5abe2f2c2922900936442eeec147e712980a1373
unknown <peter.ostertag99@gmail.com>
2023-07-17 10:44:26 -0500

    Completed getShape function

--- index.js
+++ index.js
@@ -28,8 +28,18 @@ function generateLogo() {
     ];
 }
 
-function getShape() {
+function getShape(shape) {
+    const shapeEl = {
+        circle: '<circle cx="100" cy="100" r="50"/>',
+        triangle: '<polygon points="150,20 250,180 50,180/>',
+        square: '<rect x="50" y="50" width="200" height="100"/>',
+    };
 
+    if (shape in shapeEl) {
+        return shapeEl[shape];
+    } else {
+        throw new Error('Invalid shape')
+    }
 }
 
 // Function call
